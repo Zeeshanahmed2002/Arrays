17574447ef780345ccf2433ccc3b5327cc5611b9
Zeeshan Ahmed <55400807+Zeeshanahmed2002@users.noreply.github.com>
2019-11-06 19:23:10 +0500

    Add files via upload

---
+++ Bubble Sorting.vb
@@ -0,0 +1,34 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NUM(8) As Integer
+        Dim i As Integer = 0
+        Dim x As String = ""
+        Dim y As Integer = 0
+        Dim z As Integer = 0
+
+        Console.Write("Enter your values for Array = ")
+        For i = 1 To 8
+            x = Console.ReadLine
+            NUM(i) = x
+        Next
+
+        For j = 7 To 0 Step -1
+            For i = 1 To j
+                If NUM(i) > NUM(i + 1) Then
+                    y = NUM(i)
+                    NUM(i) = NUM(i + 1)
+                    NUM(i + 1) = y
+                End If
+            Next
+        Next
+        Console.WriteLine("The values of the array in Assending order is = ")
+        For i = 1 To 8
+            z = NUM(i)
+            Console.WriteLine(z)
+        Next
+        Console.ReadKey()
+
+    End Sub
+
+End Module
---
+++ Finding value in Array.vb
@@ -0,0 +1,40 @@
+﻿Module Module1
+
+    Sub Main()
+        Dim NUM(8) As Integer
+        Dim i As Integer = 0
+        Dim x As String = ""
+        Dim ISFOUND As Boolean = False
+        Dim y As Integer = 0
+
+
+        Console.Write("Enter your values for Array = ")
+        For i = 1 To 8
+            x = Console.ReadLine
+            NUM(i) = x
+        Next
+
+        i = 0
+        Console.Write("Enter the value to find = ")
+        y = Console.ReadLine
+        ISFOUND = False
+
+        For i = 1 To 8
+            If NUM(i) = y Then
+                ISFOUND = True
+                Console.WriteLine("The value has been found")
+                Console.Write("The position of the value in Array is = " & i)
+                Exit For
+            End If
+        Next
+
+        If ISFOUND = False Then
+            Console.WriteLine("The value has NOT been found")
+        End If
+
+        Console.ReadKey()
+
+    End Sub
+
+
+End Module
